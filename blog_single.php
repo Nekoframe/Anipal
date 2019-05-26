412f099b9ae8b360418858de7445fcd7c4c0164d
Nekoframe <34092531+Nekoframe@users.noreply.github.com>
2019-05-26 16:57:32 +0100

    20190526 additional stuff on blog_single page

--- blog_single.php
+++ blog_single.php
@@ -14,24 +14,6 @@
 	<!-- end blog__banner-wrapper -->
 
 
-	<!-- blog__wrapper -->
-<!-- 	<div class="blog__wrapper blog__page" id="blog">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="section__heading">
-						<p class="subtitle">Something</p>
-						<h2>We want to share</h2>
-					</div>
-				</div>
-			</div>
-
-			
-		</div>
-	</div> -->
-	<!-- end blog__wrapper -->
-
-
 	<!-- article__wrapper -->
 	<div class="article__wrapper">
 		<div class="container">
@@ -61,16 +43,83 @@ adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dol
 nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
 voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
 
+						<blockquote>
+							<p>An animal’s eyes have the power to speak a great language.<span> - Martin Buber</span></p>
+						</blockquote>
+
 						<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
 veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
 consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
 adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
 nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
 voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur</p>
+
+						<ul>
+							<li>Sed ut perspiciatis unde omnis iste natus</li>
+							<li>Sed ut perspiciatis unde omnis iste natus</li>
+							<li>Sed ut perspiciatis unde omnis iste natus</li>
+							<li>Sed ut perspiciatis unde omnis iste natus</li>
+							<li>Sed ut perspiciatis unde omnis iste natus</li>
+						</ul>
 					</div>
 				</div>
 			</div>
 
+			<!-- social__box -->
+			<div class="social__box">
+				<div class="social">
+					<a href="#!">
+						<i class="fab fa-facebook-f"></i>
+					</a>
+				</div>
+				<div class="social">
+					<a href="#!">
+						<i class="fab fa-facebook-messenger"></i>
+					</a>
+				</div>
+				<div class="social">
+					<a href="#!">
+						<i class="fab fa-instagram"></i>
+					</a>
+				</div>
+				<div class="social">
+					<a href="#!">
+						<i class="fab fa-line"></i>
+					</a>
+				</div>
+				<div class="social">
+					<a href="#!">
+						<i class="fab fa-twitter"></i>
+					</a>
+				</div>
+			</div>
+			<!-- end social__box -->
+
+
+			<!-- author__box -->
+			<div class="author__box">
+				<hr>
+				<div class="container-fluid auther__container">
+					<div class="row">
+						<div class="col-md-4">
+							<figure>
+								<img src="img/author.jpg" class="author__image" alt="Yuko Ishikawa">
+							</figure>
+						</div>
+						<div class="col-md-8">
+							<div class="author__detail">
+								<h2>Yuko Ishikawa</h2>
+								<strong>MSc. Animal Behaviour, University of Lincoln.</strong>
+								<p>Animal lover from Japan who has a lot of technician experience working with animal hospital and animal medical company. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- end author__box -->
+
+
+			<!-- button__box -->
 			<div class="button__box">
 				<button class="btn btn--light btn--prev">
 					<i class="fas fa-chevron-left"></i>
@@ -80,6 +129,7 @@ voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum
 					<i class="fas fa-chevron-right"></i>
 				</button>
 			</div>
+			<!-- end button__box -->
 		</div>
 	</div>
 	<!-- end article__wrapper -->
